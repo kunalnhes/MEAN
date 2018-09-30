d9c218899739ea90de994011ec3ef718ecd9ffeb
kunalnhes <k.kunalj@outlook.com>
2018-09-30 18:22:53 +0530

    angular going on

--- VideoPlayer/server.js
+++ VideoPlayer/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use('/api',api);
 
 app.get('*',(req,res) => {
-  res.sendFile(path.join(__dirname,'dist/VideoPlayer/index.html'));
+  res.sendFile(path.join(__dirname,'dist/index.html'));
 });
 
 app.listen(port,function(){
